44ca4554ae4d28c7c1f75514fd3eca0865324721
Meabh <91346624+MeabhG97@users.noreply.github.com>
2023-03-25 22:55:07 +0000

    Artist DAO Interface add new.

--- src/main/java/meabh/DAO/ArtistDaoInterface.java
+++ src/main/java/meabh/DAO/ArtistDaoInterface.java
@@ -7,5 +7,6 @@ import meabh.DTO.Artist;
 public interface ArtistDaoInterface {
     public List<Artist> findAllArtists();
     public Artist findArtistById(int id);
-    public boolean deleteArtistById(int id);
+    public void deleteArtistById(int id);
+    public void addArtist(Artist artist);
 }
