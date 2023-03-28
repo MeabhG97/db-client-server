b1e704237116b12099066b253a7e3cb172d97b74
Meabh <91346624+MeabhG97@users.noreply.github.com>
2023-03-28 12:49:17 +0100

    Artist Formation Date comparator

---
+++ src/main/java/meabh/Comparators/ArtistSortByFormedDate.java
@@ -0,0 +1,14 @@
+package meabh.Comparators;
+
+import java.util.Comparator;
+
+import meabh.DTO.Artist;
+
+public class ArtistSortByFormedDate implements Comparator<Artist> {
+    //Returns 0 if equal, Positive if o1 is later than o2, Negative if o1 is earlier than o2
+    @Override
+    public int compare(Artist o1, Artist o2) {
+        return o1.getFormation().compareTo(o2.getFormation());
+    }
+
+}
