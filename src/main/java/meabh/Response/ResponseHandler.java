a51a6931c52af077cc6f98f56daee6039378a493
Meabh <91346624+MeabhG97@users.noreply.github.com>
2023-04-13 16:30:56 +0100

    Response Handler

---
+++ src/main/java/meabh/Response/ResponseHandler.java
@@ -0,0 +1,13 @@
+package meabh.Response;
+
+import com.google.gson.Gson;
+
+public class ResponseHandler {
+    private static Gson gson = new Gson();
+    
+    public static void Response(String responseJson){
+        Response response = gson.fromJson(responseJson, Response.class);
+        System.out.println(response.getCode());
+        System.out.println(response.getBody());
+    }
+}
