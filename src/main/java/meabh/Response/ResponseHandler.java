5ed0f09b011252f0c671b96ceb67acd363ea7d8b
Meabh <91346624+MeabhG97@users.noreply.github.com>
2023-04-16 18:20:33 +0100

    Changed the name of method to handleResponse

--- src/main/java/meabh/Response/ResponseHandler.java
+++ src/main/java/meabh/Response/ResponseHandler.java
@@ -5,7 +5,7 @@ import com.google.gson.Gson;
 public class ResponseHandler {
     private static Gson gson = new Gson();
     
-    public static void Response(String responseJson){
+    public static void handleResponse(String responseJson){
         Response response = gson.fromJson(responseJson, Response.class);
         System.out.println(response.getCode());
         System.out.println(response.getBody());
